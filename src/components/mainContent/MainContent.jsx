6facc65fc854987122c147fc2cb2119ccc160d47
Lucas Araújo <lucas-dev>
2023-02-03 13:10:32 -0300

    Content text

--- src/components/mainContent/MainContent.jsx
+++ src/components/mainContent/MainContent.jsx
@@ -6,16 +6,24 @@ import './index.scss';
 
 
 const component = <InputPassword />
-const description = "Componente para um validação de senha no front, onde aparece uma lista na qual a medida que o campo vai sendo preenchido, a lista vai sendo atualizada e cada item sendo atualizado se for de acordo com a regra."
+const description = "Componente para validar senha apenas no front, onde aparece uma lista na qual a medida que o campo vai sendo preenchido, a lista vai sendo atualizada e cada item sendo atualizado se for de acordo com a regra."
 const title = "Básico";
+
+const componentII = <InputPassword />
+const descriptionII = "Componente para validar senha apenas no front. Aditivo de um ícone clicável, na qual o conteúdo digitado na senha pode se tornar visível."
+const titleII = "Senha visível";
+
+const componentIII = <InputPassword />
+const descriptionIII = "Componente para validar senha apenas no front. Aditivo de um validador, no qual após a senha ser validada. Retorna uma mensagem de sucesso ou os informa os items que não foram preenchidos na lista."
+const titleIII = "Com validação";
 
 export function MainContent() {
     return (
         <div className="main-content">
             <div className="card-row">
                 <CardPresentation component={component} description={description} title={title}></CardPresentation>
-                <CardPresentation component={component} description={description} title={title}></CardPresentation>
-                <CardPresentation component={component} description={description} title={title}></CardPresentation>
+                <CardPresentation component={componentII} description={descriptionII} title={titleII}></CardPresentation>
+                <CardPresentation component={componentIII} description={descriptionIII} title={titleIII}></CardPresentation>
             </div >
         </div >
     )
