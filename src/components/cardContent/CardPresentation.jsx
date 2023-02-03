fab3c846e36e748311b3ad766d4219a7e379ecb6
Lucas Araújo <lucas-dev>
2023-02-02 23:40:41 -0300

    estilzar card

---
+++ src/components/cardContent/CardPresentation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './style.scss';
+
+export function CardPresentation() {
+    return (
+        <div className="main-content">
+            <div className="card-presentation">
+                <div className="header">
+                    <i className="fab fa-react"></i>
+                    <p>Componente para campo de senha</p>
+                </div>
+            </div>
+        </div>
+    )
+}
