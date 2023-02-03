4aa5b489e850c998f86e95b00ed8b5e8c5801357
Lucas Araújo <lucas-dev>
2023-02-03 14:15:23 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-
 import './index.css';
-import { MainContent } from './components/mainContent/MainContent';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <MainContent></MainContent>
+        <App></App>
     </React.StrictMode>
 );
 
---
+++ src/routes/routes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainContent } from "../components/mainContent/MainContent";
+import { NavbarSystem } from "../components/navbarSystem/navbarSystem";
+
+export function RouteSystem() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<NavbarSystem />}>
+                    <Route index element={<MainContent />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
