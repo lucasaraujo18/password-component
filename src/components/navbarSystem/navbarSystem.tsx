33f2bd9883a72da606163a84319c15c93943fa89
Lucas Araújo <lucas-dev>
2023-02-03 14:02:08 -0300

    navbar

---
+++ src/components/navbarSystem/navbarSystem.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "../../../public/logo192.png";
+import { Outlet, Link } from "react-router-dom";
+
+import "./style.scss";
+
+export function NavbarSystem() {
+    return (
+        <>
+            <div className="nav-bar d-flex justify-content-between">
+                <ul>
+                    <Link to="/">
+                        <li>
+                            <img
+                                src={logo}
+                                className="nav-bar-logo"
+                                alt="logo"
+                            />
+                        </li>
+                    </Link>
+                    <Link to="play" className="text-white">
+                        <li>
+                            Jogar <i className="fab fa-react"></i>
+                        </li>
+                    </Link>
+                    <li>
+                        Feito com <i className="fab fa-react"></i>
+                        <i className="fab fa-react"></i>
+                        <i className="fab fa-react"></i>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Criar conta <i className="fab fa-react"></i>
+                    </li>
+                    <li>
+                        Entrar <i className="fab fa-react"></i>
+                    </li>
+                </ul>
+            </div>
+
+            <Outlet />
+        </>
+    );
+}
